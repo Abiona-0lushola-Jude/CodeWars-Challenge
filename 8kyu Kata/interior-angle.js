5c44c38981bdf98cd236c6f7d722603b4938135e
Abiona Olushola Jude <103720345+Abiona-0lushola-Jude@users.noreply.github.com>
2022-05-27 13:04:24 +0100

    Add files via upload

---
+++ 8kyu Kata/interior-angle.js
@@ -0,0 +1,12 @@
+//  Questions
+
+// You are given two interior angles (in degrees) of a triangle.
+
+// Write a function to return the 3rd.
+
+// Note: only positive integers will be tested.
+
+// Solutions
+function otherAngle(a, b) {
+    return 180 - (a+b)
+   }
---
+++ 8kyu Kata/reverse-sequence.js
@@ -0,0 +1,16 @@
+//  Question
+
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+
+
+// Solution
+
+const reverseSeq = n => {
+    let result = []
+    for(i=1;i<=n;i++){
+      result.push(i)
+    }
+    return result.sort(function (a,b){return b -a})
+}
